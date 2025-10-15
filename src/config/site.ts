06f879ea0e20e5000e56926e592ce18f3dd3d4db
Kristen Tonga <kristen.tonga@gmail.com>
2025-10-15 22:11:22 +0100

    Added every.org donate button throughout the site and created general config file

---
+++ src/config/site.ts
@@ -0,0 +1,17 @@
+export const SITE_CONFIG = {
+  name: 'Open Tech International',
+  shortName: 'OTI',
+  description: 'Open Tech International - Advancing technology for global water solutions',
+  location: 'Alaska, USA',
+  
+  // URLs
+  donateUrl: 'https://www.every.org/open-tech-international?theme_color=053f52&utm_campaign=donate-link#/donate',
+  
+  // Social Media
+  social: {
+    linkedin: 'https://linkedin.com/company/opentechinternational',
+    github: 'https://github.com/OpenTechInternational',
+    twitter: 'https://twitter.com/OpenTechIntl',
+  },
+} as const;
+
